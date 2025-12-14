19e08b143b15fa3d6c32eff5aec75c84a14b39b7
Iuri <iuri.granjeiro2@gmail.com>
2025-12-14 11:36:50 -0300

    Moved tables, current and future, to a folder

--- conditional_file_stream.cpp
+++ conditional_file_stream.cpp
@@ -1,22 +1,23 @@
-#include <string>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 // Writes to a file only if enabled.
 struct ConditionalFileStream {
-    std::fstream output_file;
-    bool enabled;
+  std::fstream output_file;
+  bool enabled;
 
-    ConditionalFileStream(std::string file_name, bool en)
-        : enabled(en) {
-            if (!enabled) return;
-            output_file.open(file_name, std::ios::out | std::ios::trunc);
-        }
+  ConditionalFileStream(std::string file_name, bool en) : enabled(en) {
+    if (!enabled)
+      return;
+    output_file.open("F:\\Documents\\EGZ-Solver\\EGZ-Solver\\Experimental tables\\" + file_name, std::ios::out | std::ios::trunc);
+  }
 
-    template<typename T>
-    ConditionalFileStream& operator<<(const T& v) {
-        if (enabled) output_file << v;
-        return *this;
-    }
+  template <typename T>
+  ConditionalFileStream &operator<<(const T &v) {
+    if (enabled)
+      output_file << v;
+    return *this;
+  }
 };
