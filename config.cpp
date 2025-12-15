111681dbbea5abf5e967d96817f9cff27e7b3f32
Iuri <iuri.granjeiro2@gmail.com>
2025-12-15 18:02:54 -0300

    Expanded F4

--- config.cpp
+++ config.cpp
@@ -7,6 +7,6 @@ long long smallestPowerBiggerThan(long long base, long long value) {
 }
 
 // Testing for all values of m < M_MAX and T_MIN(m) <= t < T_MAX(m)
-const int M_MAX = 30;
+const int M_MAX = 50;
 int T_MIN(int m = 1) { return m; }
-int T_MAX(int m = M_MAX) { return 40 /*smallestPowerBiggerThan(2, m) + m + 1*/; }
+int T_MAX(int m = M_MAX) { return 60 /*smallestPowerBiggerThan(2, m) + m + 1*/; }
